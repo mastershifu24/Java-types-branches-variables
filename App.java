690f5e28a93c0a5bbfb392bfb968e119fd2da6f4
Ahmed Shifa <ahmedshifa298@gmail.com>
2023-03-19 14:52:12 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+        System.out.println("My ___friend enjoys ___.");
+        System.out.println("My charismatic friend enjoys coffee.");
+
+        String username = "Ahmed";
+        System.out.println(System.out.println("Hello, World"));
+    }
+}
